514dc21cda28a9a99b6401aa81f7fb59ca988636
matthew <larochelle.matthew@gmail.com>
2023-07-21 15:43:23 -0700

    frontend count all set

--- api/index.js
+++ api/index.js
@@ -9,6 +9,7 @@ import hotelsRoute from './routes/hotels.js';
 import roomsRoute from './routes/rooms.js';
 import usersRoute from './routes/users.js';
 import cookieParser from 'cookie-parser';
+// import cors from 'cors'
 
 
 
@@ -32,6 +33,7 @@ mongoose.connection.on("connected", ()=>{
 })
 
 // middlewares
+// app.use(cors())
 app.use(cookieParser())
 app.use(express.json())
 
