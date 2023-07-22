8429cec8383e983f97968e1d935dd2dbaf4c4b40
matthew <larochelle.matthew@gmail.com>
2023-07-21 17:05:56 -0700

    stopping for today

--- client/src/components/propertyList/PropertyList.jsx
+++ client/src/components/propertyList/PropertyList.jsx
@@ -6,7 +6,6 @@ const PropertyList = () => {
   const {data, loading, error} = useFetch(
     "/hotels/countByType"
     );
-    console.log("data", data)
 
   const images = [
     "https://cf.bstatic.com/xdata/images/xphoto/square300/57584488.webp?k=bf724e4e9b9b75480bbe7fc675460a089ba6414fe4693b83ea3fdd8e938832a6&o=",
@@ -22,7 +21,6 @@ const PropertyList = () => {
         "Loading" 
        ) : ( 
           <>
-          {/* I added this line of code in */}
           {data &&
             images.map((img, i) => (
             <div className="pListItem" key={i}>
