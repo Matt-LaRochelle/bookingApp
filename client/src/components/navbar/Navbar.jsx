229d0ed662659acc27c2215d133ca89aa04e004b
matthew <larochelle.matthew@gmail.com>
2023-07-27 11:08:02 -0700

    login auth complete

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,18 +1,30 @@
 import "./navbar.css"
 import { Link } from 'react-router-dom'
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
+
 
 const Navbar = () => {
+  const { user, dispatch } = useContext(AuthContext);
+
+  const handleClick = () => {
+    dispatch({type: 'LOGOUT'})
+  }
+
   return (
     <div className="navbar">
       <div className="navContainer">
       <Link to='/' style={{color: "inherit", textDecoration:"none"}}>
         <span className="logo">lamabooking</span>
       </Link>
-        
-        <div className="navItems">
-          <button className="navButton">Register</button>
-          <button className="navButton">Login</button>
-        </div>
+        {user ? user.username : (
+          <div className="navItems">
+            <button className="navButton">Register</button>
+            <button className="navButton">Login</button>
+          </div>
+        )}
+        {/* I am adding a logout button */}
+        {user && <button onClick={handleClick}>Logout</button>}
       </div>
     </div>
   )
