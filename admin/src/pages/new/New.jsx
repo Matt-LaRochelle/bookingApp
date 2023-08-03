0c9fd30e4f36de1bd305c00e7e0e4976300e8b3e
matthew <larochelle.matthew@gmail.com>
2023-08-03 10:04:00 -0700

    adding new user works

--- admin/src/pages/new/New.jsx
+++ admin/src/pages/new/New.jsx
@@ -3,17 +3,37 @@ import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
 import DriveFolderUploadOutlinedIcon from "@mui/icons-material/DriveFolderUploadOutlined";
 import { useState } from "react";
+import axios from 'axios'
 
 const New = ({ inputs, title }) => {
   const [file, setFile] = useState("");
   const [info, setInfo] = useState({});
 
   const handleChange = (e) => {
-    setInfo(prev=>({...prev, [e.target.id]: e.target.value}));
-  }
+    setInfo((prev) => ({ ...prev, [e.target.id]: e.target.value}));
+  };
 
   const handleClick = async (e) => {
     e.preventDefault();
+    const data = new FormData()
+    data.append("file", file)
+    data.append("upload_preset", "upload")
+    try {
+      const uploadResponse = await axios.post(
+        "https://api.cloudinary.com/v1_1/drxc2luq6/image/upload", 
+        data
+      );
+
+      const {url} = uploadResponse.data;
+      const newUser = {
+        ...info,
+        img: url,
+      };
+
+      await axios.post("/auth/register", newUser)
+    } catch (err) {
+      console.log(err)
+    }
 
   }
 
@@ -53,7 +73,12 @@ const New = ({ inputs, title }) => {
               {inputs.map((input) => (
                 <div className="formInput" key={input.id}>
                   <label>{input.label}</label>
-                  <input onChange={handleChange} type={input.type} placeholder={input.placeholder} />
+                  <input 
+                    onChange={handleChange} 
+                    type={input.type} 
+                    placeholder={input.placeholder}
+                    id={input.id}
+                  />
                 </div>
               ))}
               <button onClick={handleClick}>Send</button>
